db84a27bc72b804595fdda70e142c6e84b0b366e
m4li <mak.2898@gmail.com>
2014-12-13 22:33:51 +0200

    Create hesap makinesi sürekli.py

---
+++ hesap makinesi sürekli.py
@@ -0,0 +1,33 @@
+# -*- coding: cp1254 -*-
+# PYTHON'DA SÜREKLİ ÇALIŞAN HESAP MAKİNESİ
+from __future__ import division
+a=1
+while a==1:
+    print"""
+    TOPLAMA [1]
+    ÇIKARMA [2]
+    ÇARPMA  [3]
+    BÖLME   [4]
+    """
+    islem = raw_input(" LUTFEN YAPACAGINIZ ISLEMIN NUMARASINI GIRINIZ:")
+    if islem == "1":
+        print"TOPLAMA ISLEMINI SEÇCTINIZ!"
+        sayi1 = int(raw_input(" ILK SAYIYI GIRINIZ:"))
+        sayi2 = int(raw_input(" IKINCI SAYIYI GIRINIZ:"))
+        print sayi1,"+",sayi2,"=",sayi1+sayi2
+    if islem == "2":
+        print"CIKARMA ISLEMINI SEÇCTINIZ!"
+        sayi1 = int(raw_input(" ILK SAYIYI GIRINIZ:"))
+        sayi2 = int(raw_input(" IKINCI SAYIYI GIRINIZ:"))
+        print sayi1,"-",sayi2,"=",sayi1-sayi2
+    if islem == "3":
+        print"CARPMA ISLEMINI SEÇCTINIZ!"
+        sayi1 = int(raw_input(" ILK SAYIYI GIRINIZ:"))
+        sayi2 = int(raw_input(" IKINCI SAYIYI GIRINIZ:"))
+        print sayi1,"x",sayi2,"=",sayi1*sayi2
+    if islem == "4":
+        print"BOLME ISLEMINI SEÇCTINIZ!"
+        sayi1 = int(raw_input(" ILK SAYIYI GIRINIZ:"))
+        sayi2 = int(raw_input(" IKINCI SAYIYI GIRINIZ:"))
+        print sayi1,"/",sayi2,"=",sayi1/sayi2
+
